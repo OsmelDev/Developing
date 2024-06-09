88c7540fe0b20520a8525486457bab05b59d5e21
OsmelDev <oprades4@gmail.com>
2024-06-08 23:19:32 -0400

    Agregue la funcionabilidad de recibir la Ip del usuario y la funcionabilidad de mostar desde que pais esta conectado en el archivo etch.js, del mismo modo recoji los datos en el componente Header para mostrar la ciudad desde donde se conecta

--- shoppin_cart/src/hooks/fetch.js
+++ shoppin_cart/src/hooks/fetch.js
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { API_URL, API_URL_CATEGORIES } from '../constant/constant';
+import { API_GET_IP, API_LOCATION, API_URL, API_URL_CATEGORIES } from '../constant/constant';
 import { product } from '../assets/products.json';
 
 export const loadProduct = () => {
@@ -14,17 +14,42 @@ export const loadProduct = () => {
       .finally(() => setLoading(false));
   });
   const localProducts = product;
+
   const getCategories = useCallback(() => {
     fetch(API_URL_CATEGORIES)
       .then((res) => res.json())
       .then((data) => setCategories(data))
-      .finally(()=>setLoading(false));
+      .finally(() => setLoading(false));
   });
+
   useEffect(() => {
     setLoading(true);
     loadData();
     getCategories();
   }, []);
 
-  return { data,categories, localProducts, loading };
+  return { data, categories, localProducts, loading };
+};
+
+export const getUserIp = () => {
+  const [IP, setIP] = useState();
+  const getData = useCallback(() => {
+    fetch(`${API_GET_IP}`)
+      .then((resp) => resp.json())
+      .then((data) => setIP(data.ip));
+  });
+  useEffect(() => {
+
+    getData();
+  }, []);
+  return { IP };
+};
+
+export const getCoutryUser = () => {
+  const { IP } = getUserIp();
+  const [country, setCountry] = useState();
+  fetch(`${API_LOCATION}/${IP}`)
+    .then((resp) => resp.json())
+    .then((data) => setCountry(data.country));
+  return { country };
 };
