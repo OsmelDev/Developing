499c00bc56000400eaacb46466ef806c3a62b3cc
OsmelDev <oprades4@gmail.com>
2024-06-10 23:42:28 -0400

    agregue un nuevo paquete para implementar el multilenguaje, en componente Header agregue dos opciones para cambiar el lenguaje en dicho componente por ahora, dichas opciones se hicieron con una etiqueta span y img simulando los botones, y se implemento la funcion para cambiar los ideomas

--- shoppin_cart/src/components/Header.jsx
+++ shoppin_cart/src/components/Header.jsx
@@ -6,12 +6,19 @@ import { GrLocation } from 'react-icons/gr';
 import Cart from './Cart';
 import { FilterContext } from '../context/filterContext';
 import { loadProduct } from '../hooks/fetch';
-import { TiArrowSortedDown } from 'react-icons/ti';
+import en from '../assets/images/en.svg';
+import es from '../assets/images/es.svg';
+
+import { useTranslation } from 'react-i18next';
+
+
 
 const Header = () => {
+  const {t, i18n} = useTranslation("global");
   const [searchInput, setSearchInput] = useState('');
   const { filter, setFilter } = useContext(FilterContext);
   const { categories } = loadProduct();
+
 
   const hadleChangeCategory = (event) => {
     setFilter((prevState) => ({
@@ -29,7 +36,7 @@ const Header = () => {
       </div>
 
       <span className={style.ubication}>
-        Deliver to{' '}
+          {t('header.ubication')}
         <p>
           <GrLocation />
           Cuba
@@ -39,10 +46,17 @@ const Header = () => {
       <div className={style.containerFilterSearch}>
         <div className={style.filter}>
           <select name='' id={categoryFilterId} onChange={hadleChangeCategory}>
-            <option value='all'>All </option>
-            {categories.map((category, i) => (
-              <option key={i} value={category}>{category} </option>
-            ))}
+            <option value='all'>{t("header.categories.all")} </option>
+            <option value='electronics'>{t("header.categories.electronics")} </option>
+            <option value='jewelery'>{t("header.categories.jewelery")} </option>
+            <option value="men's clothing">{t("header.categories.clothing.men's")} </option>
+            <option value="women's clothing">{t("header.categories.clothing.women's")} </option>
+
+            {/* {categories.map((category, i) => (
+              <option key={i} value={category}>
+                {category}{' '}
+              </option>
+            ))} */}
           </select>
         </div>
 
@@ -56,13 +70,24 @@ const Header = () => {
           </button>
         </div>
       </div>
+
+      <div className={style.languajeSelectorContainer}>
+        <div id={style.languajeSelector}>
+          <span className={style.languaje} onClick={()=> i18n.changeLanguage("es")}>
+            <img src={es}/> es{' '}
+          </span>
+          <span className={style.languaje} onClick={()=> i18n.changeLanguage("en")}>
+            <img src={en}/> en{' '}
+          </span>
+        </div>
+      </div>
       <span>
-        Hello, sign in <br />
-        Acconunt & List
+        {t("header.account.greeting")} <br />
+        {t("header.account.login")}
       </span>
 
       <span>
-        Returns <br />& Orders
+      {t("header.orders.returns")} <br />{t("header.orders.orders")}
       </span>
 
       <Cart />
--- shoppin_cart/src/main.jsx
+++ shoppin_cart/src/main.jsx
@@ -2,5 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import global_es from "./translations/es/global.json"
+import global_en from "./translations/en/global.json"
+
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng:"en",
+  resources:{
+    es:{
+      global:global_es
+    },
+    en:{
+      global:global_en
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <I18nextProvider i18n={i18next}>
+    <App />
+  </I18nextProvider>
+);
