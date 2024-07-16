3b6a09ebaa421883e6e5f1d284e4ef536802d8fc
OsmelDev <oprades4@gmail.com>
2024-07-15 21:42:42 -0400

    Se construyo el componente Skill y se le agregaron estilos

--- next-portafolio/app/skill/page.jsx
+++ next-portafolio/app/skill/page.jsx
@@ -1,3 +1,65 @@
+import { FaGithub } from "react-icons/fa";
+import { FaCss3 } from "react-icons/fa6";
+import { FaHtml5 } from "react-icons/fa6";
+import { FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import styles from './Skill.module.css'
+
+const skills = [
+		{
+			label:'HTML',
+			icon: <FaHtml5/>,
+			level: 'Basic'
+		},
+		{
+			label:'JavasCrypt',
+			icon: <IoLogoJavascript/>,
+			level: 'Intermediate'
+		},
+		{
+			label:'CSS',
+			icon: <FaCss3/>,
+			level: 'Basic'
+		},
+		{
+			label:'React',
+			icon: <FaReact/>,
+			level: 'Intermediate'
+		},
+		{
+			label:'GIT',
+			icon: <FaGithub/>,
+			level: 'Basic'
+		},
+	]
+
 export default function SkillPage(){
-	return(<h1>Estas son mis Skill</h1>)
+	return(
+		<section>
+			<header className={styles.sectionHeader}>
+				<h1>
+					Skills
+				</h1>
+				<small>
+					My Tecnical Level
+				</small>
+			</header>
+			<article className={styles.sectionArticle}>
+				<h1>
+					Frontend Developer
+				</h1>
+				<div>
+					<ul className={styles.listSkills}>
+						{skills.map(({label, icon,level})=>(
+							<li key={label} className={styles.skill}>
+								{icon}
+								<p>{label}</p>
+								<small>{level}</small>
+							</li>
+							))}
+					</ul>
+				</div>
+			</article>
+		</section>
+		)
 }
