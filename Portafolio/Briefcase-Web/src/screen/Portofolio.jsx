bc570a64d8123dbad70858f987f3095f224b012a
Osmel <oprades4@gmail.com>
2024-10-18 00:43:05 -0400

    otro avance

--- Portafolio/Briefcase-Web/src/screen/Portofolio.jsx
+++ Portafolio/Briefcase-Web/src/screen/Portofolio.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
-import style from './Portofolio.module.css'
+import React from "react";
+import style from "./Portofolio.module.css";
+
+const projects = [
+  {
+    name: "Amazon Clone",
+    image: "/AmazonClon.png",
+    descripcion:
+      "sitio que realize en forma de practica, simulando una tienda online",
+  },
+  {
+    name: "Ecommerce TechLab",
+    image: "/Ecommerce.png",
+    descripcion:
+      "sitio que realize en forma de practica, simulando una tienda online",
+  },
+  {
+    name: "Chat-Io",
+    image: "/ChatIo.png",
+    descripcion:
+      "sitio que realize en forma de practica, simulando una tienda online",
+  },
+];
 
 export const Portofolio = () => {
   return (
     <div className={style.portofolio} name="Portofolio">
-      <div className={style.cartEcommerce}>
-        <img src="/AmazonClon.png" alt="" />
-        <span>sitio que realize en forma de practica, simulando una tienda online</span>
-      </div>
-
-      <div className='chat'>
-        <img src="" alt="" />
-        <span>Chat en tiempo real, donde puede enviar y recibir mensajes tanto en el canal general como a traves del privado</span>
-      </div>
-      
-      <div className='chat'>
-        <img src="" alt="" />
-        <span>Chat en tiempo real, donde puede enviar y recibir mensajes tanto en el canal general como a traves del privado</span>
-      </div>
+      <h1>Projects</h1>
+      <section>
+        {projects.map((project, i) => (
+          <div className={style.cart} key={i}>
+            <div className={style.image_box}>
+              <img src={project.image} />
+            </div>
+            <div className={style.content}>
+              <h2>{project.name}</h2>
+              <p>{project.descripcion}</p>
+              <a href="#" onClick={() => alert("detalles")}>
+                Ver detalles
+              </a>
+            </div>
+          </div>
+        ))}
+      </section>
     </div>
-  )
-}
+  );
+};
--- Portafolio/Briefcase-Web/src/screen/Skill.jsx
+++ Portafolio/Briefcase-Web/src/screen/Skill.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from './Skill.module.css'
-import VerifiedRoundedIcon from '@mui/icons-material/VerifiedRounded';
+import styles from "./Skill.module.css";
+import VerifiedRoundedIcon from "@mui/icons-material/VerifiedRounded";
 
 export const Skill = () => {
   const skills = [
@@ -13,7 +13,6 @@ export const Skill = () => {
       id: "2",
       name: "JavasCrypt",
       level: "Intermediate",
-      
     },
     {
       id: "3",
@@ -33,7 +32,7 @@ export const Skill = () => {
   ];
 
   return (
-    <div name="Skill" className={styles.Skill}>
+    <div name="Skill" className={styles.skill}>
       <div className={styles.topic}>
         <h1>Skills</h1>
         <p>My technical level</p>
@@ -41,15 +40,16 @@ export const Skill = () => {
       <div className={styles.skillsContainer}>
         <h2>Frontend Developer</h2>
         <ul className={styles.skillList}>
-          {skills.map((skill ) => (
-            
-          <li key={skill.id}>
-           
-            <p><VerifiedRoundedIcon className={styles.Icon}/> 
-            {skill.name}</p>
-            <span>{skill.level}</span>
-          </li>)
-        )}</ul>
+          {skills.map((skill) => (
+            <li key={skill.id}>
+              <p>
+                <VerifiedRoundedIcon className={styles.Icon} />
+                {skill.name}
+              </p>
+              <span>{skill.level}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
